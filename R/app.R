236a9f6be6289cb352d068d05669c6d86134ce6e
Valavi, Roozbeh (Environment, Clayton) <Roozbeh.Valavi@csiro.au>
2024-09-22 18:57:52 +1000

    trying shiny.fluent

--- R/app.R
+++ R/app.R
@@ -2,20 +2,60 @@ library(shiny)
 library(shiny.fluent)
 library(semantic.dashboard)
 library(mapgl)
+library(spocc)
+library(rgbif)
+library(sf)
 
+# species_data <- spocc::occ(
+#   query = "Phascolarctos cinereus",
+#   from = "gbif",
+#   limit = 1000
+#   # geometry = sf_bbox(cat_ext)
+# )
+
+get_data <- function(x) {
+  return(
+    x$gbif$data[[1]]
+  )
+}
+  
+pnts <- sf::st_as_sf(get_data(species_data), coords = c("longitude", "latitude"), crs = 4326)
+  
 
 ui <- dashboardPage(
-  dashboardHeader(title = "Species map"),
+  dashboardHeader(title = "Species Dashboard"),
   dashboardSidebar(
+    size = "",
     sidebarMenu(
-      menuItem(tabName = "hope", text = "Map page", icon = icon("map")),
+      menuItem(tabName = "get", text = "Species detection", icon = icon("leaf")),
+      menuItem(tabName = "mapage", text = "Map page", icon = icon("map")),
       menuItem(tabName = "desc", text = "Description", icon = icon("file alternate"))
     )
   ),
   
   dashboardBody(
-    fluidRow(
-        maplibreOutput("map", height = "700px")
+    tabItems(
+      tabItem(tabName = "get",
+              DefaultButton.shinyInput(
+                inputId = ("button_1"),
+                text = "Get species",
+                primary = FALSE,
+                split = TRUE,
+                splitButtonAriaLabel = "See 2 options",
+                `aria-roledescription` = "split button",
+                # menuProps = menuProps,
+                disabled = FALSE,
+                checked = FALSE
+              )
+      ),
+      tabItem(tabName = "mapage",
+              fluidRow(
+                maplibreOutput("map", height = "700px")
+              )
+      ),
+      tabItem(tabName = "desc",
+              h2("Description page"),
+      )
     )
   )
 
@@ -25,12 +65,13 @@ ui <- dashboardPage(
 server <- function(input, output) {
   
   output$map <- renderMaplibre({
-    maplibre(style = carto_style("positron"))
-      # fit_bounds(nc, animate = FALSE) |> 
-      # add_fill_layer(id = "nc_data",
-      #                source = nc,
-      #                fill_color = "blue",
-      #                fill_opacity = 0.5)
+    maplibre(style = carto_style("positron")) |>
+      fit_bounds(pnts, animate = FALSE) |>
+      add_circle_layer(
+        id = "sp_data",
+        circle_color = scales::alpha("black", 0.3),
+        source = pnts,
+      )
   })
 
 }
