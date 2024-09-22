045ced3a870789993945ece712226cdc124e4bee
Valavi, Roozbeh (Environment, Clayton) <Roozbeh.Valavi@csiro.au>
2024-09-22 18:27:57 +1000

    init maplibre

--- R/app.R
+++ R/app.R
@@ -1,4 +1,38 @@
 library(shiny)
+library(shiny.fluent)
+library(semantic.dashboard)
+library(mapgl)
 
 
+ui <- dashboardPage(
+  dashboardHeader(title = "Species map"),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem(tabName = "hope", text = "Map page", icon = icon("map")),
+      menuItem(tabName = "desc", text = "Description", icon = icon("file alternate"))
+    )
+  ),
+  
+  dashboardBody(
+    fluidRow(
+        maplibreOutput("map", height = "700px")
+    )
+  )
 
+)
+
+
+server <- function(input, output) {
+  
+  output$map <- renderMaplibre({
+    maplibre(style = carto_style("positron"))
+      # fit_bounds(nc, animate = FALSE) |> 
+      # add_fill_layer(id = "nc_data",
+      #                source = nc,
+      #                fill_color = "blue",
+      #                fill_opacity = 0.5)
+  })
+
+}
+
+shinyApp(ui, server)
